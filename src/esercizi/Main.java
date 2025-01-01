2246fde1773254812b68d3cdf10f5f826cc970c6
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2025-01-01 22:42:18 +0100

    Exercise Published

---
+++ src/esercizi/Main.java
@@ -0,0 +1,17 @@
+package esercizi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/* In questo esercizio devi leggere una matrice da un file
+		 * utilizzando un file reader e uno string buffer, string builder. 
+		 * Gestire il tutto utilizzando il try-catch exeptions e 
+		 * un'enumerazione per il ritorno di sigle per ogni errore
+		 * 
+		 * Cerca la traccia dell'esercizio nella email*/
+
+	}
+
+}
