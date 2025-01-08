dd98636eaa9ca89f4c650e69f7e769f60e85513e
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2025-01-09 00:29:47 +0100

    Added txt test files

--- src/esercizi/Main.java
+++ src/esercizi/Main.java
@@ -3,17 +3,8 @@ package esercizi;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		/* In questo esercizio devi leggere una matrice da un file
-		 * utilizzando un file reader e uno string buffer, string builder. 
-		 * Gestire il tutto utilizzando il try-catch exeptions e 
-		 * un'enumerazione per il ritorno di sigle per ogni errore
-		 * 
-		 * Cerca la traccia dell'esercizio nella email*/
 		
 		StampaMatriceDaFile smdf = new StampaMatriceDaFile("src/files/matrice.txt", 3, 3);
-
 	}
 
 }
--- src/esercizi/StampaMatriceDaFileTest.java
+++ src/esercizi/StampaMatriceDaFileTest.java
@@ -20,5 +20,11 @@ class StampaMatriceDaFileTest {
 	void test() {
 		fail("Not yet implemented");
 	}
+	
+	// TEST DA FARE
+	//TODO  ->  "INVALID-FILE-NAME" se non viene trovato il file con il nome fornito
+	//TODO  ->  "INVALID-DATA" in caso di dimensioni incongruenti rispetto a quelle fornite
+	//TODO  ->  "NOT-NUMERIC-VALUE" in caso di valore non numerico presente nel file
+	//TODO  ->  In caso positivo la matrice viene stampata sotto forma di stringa
 
 }
